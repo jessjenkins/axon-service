e13112375dd33166251932cb9dee30791d7c0c9f
Jessica Jenkins <jess@jessjenkins.uk>
2020-02-11 09:00:15 +0000

    Add basic spring boot app

---
+++ monkeys-service/src/main/java/uk/jessjenkins/monkeys/service/MonkeysApplication.java
@@ -0,0 +1,11 @@
+package uk.jessjenkins.monkeys.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeysApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeysApplication.class, args);
+    }
+}
